9a5dc40c28f210d446d14a915569833b6dbe8267
claudneia <164636272+claudneia@users.noreply.github.com>
2024-06-11 20:24:21 -0300

    Add files via upload
    código finalizado

--- src/Parsing01.java
+++ src/Parsing01.java
@@ -1,5 +1,15 @@
 public class Parsing01 {
     public static void main(String[] args) {
+       String shirtPrice = "15";
+       String taxRate = "0.05";
+       String gibberish = "887ds7nds87dsfs";
+       
+       int price = Integer.parseInt(shirtPrice);
+       double rate = Double.parseDouble(taxRate);
+       System.out.println("O valor do imposto é: "+(price*rate));
+       
+       //int rate = Integer.parseInt(taxRate); foi tranformado como cometário pq o código está errado
+       int gib = Integer.parseInt(gibberish);// o exercício não pede para arrumar o erro
         //Declare and intitialize 3 Strings: shirtPrice, taxRate, and gibberish
         
         
